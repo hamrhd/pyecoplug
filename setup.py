bda367eac88534e2814b048afd9226af6249150e
hamrhd <hamrhd@gmail.com>
2025-07-29 10:04:19 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(name='pyecoplug',
         version='0.0.6',
         description='Interface for the EcoPlug outlet',
-        url='http://github.com/hamrhd/pyecoplug',
+        url='http://github.com/philburr/pyecoplug',
         author='phil.burr@gmail.com',
         license='MIT',
         packages=['pyecoplug'],
